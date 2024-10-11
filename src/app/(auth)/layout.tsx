e5d85f4de46df27929ea5dd3e3d8be2fbb738229
Veles Bh <94125529+velesbh@users.noreply.github.com>
2024-10-11 21:18:15 +0100

    Update layout.tsx

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -12,11 +12,10 @@ export default async function AuthLayout({ children }: AuthLayoutProps) {
 
     return (
         <>
-                <div className="absolute z-50  -translate-x-2/4 -translate-y-2/4 sm:-translate-y-[40%] left-2/4 top-2/4 w-full px-4 sm:px-0">
-                    {children}
-                </div>
-                <SiteFooter />
+            <div className="absolute z-50 -translate-x-2/4 -translate-y-2/4 sm:-translate-y-[40%] left-2/4 top-2/4 w-full px-4 sm:px-0">
+                {children}
             </div>
+            <SiteFooter />
         </>
     )
 }
