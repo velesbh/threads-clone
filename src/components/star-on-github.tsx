2deeec29fd20c19fa4e0277c463bef9878a43e11
Veles Bh <94125529+velesbh@users.noreply.github.com>
2024-10-11 21:05:58 +0100

    Update star-on-github.tsx

--- src/components/star-on-github.tsx
+++ src/components/star-on-github.tsx
@@ -3,11 +3,11 @@ import { Icons } from '@/components/icons'
 
 export default function StarOnGithub() {
     return (
-        <a href='https://github.com/sujjeee/threads-clone' target='_blank'>
+        <a href='https://discord.gg/M4Dz3Gj5tR'>
             <div className='hidden xl:flex min-w-[8rem]  border border-border bg-transparent p-1 rounded-full text-[14px] py-4 px-6 text-muted-foreground shadow-lg font-medium tracking-wide hover:scale-105 active:scale-95 cursor-pointer select-none transform transition-all duration-150 ease-out'>
                 <span className='flex justify-center items-center'>
                     <Icons.gitHub className='mr-2 w-4 h-4' />
-                    Star on Github
+                    We are still in beta
                 </span>
             </div>
         </a>
